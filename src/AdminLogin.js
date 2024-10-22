eb51c13a4ac6d75f01fcf10156092a41b111e6e3
SanduniWathsalya <sanduniwathsalya0620@gmail.com>
2024-10-22 19:29:01 +0530

    Delete src directory

--- src/AdminLogin.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Login from './Login';
-
-const AdminLogin = () => {
-  return <Login role="admin" />;
-};
-
-export default AdminLogin;
--- src/StudentLogin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Login from './Login';
-
-
-const StudentLogin = () => {
-  return <Login role="student" />;
-};
-
-export default StudentLogin;
